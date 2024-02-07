160c28ec80e8c703349db2550966c6fb56b47e8a
cagri <cyucel@gmail.com>
2024-02-07 19:50:19 +0000

    Initial commit

---
+++ ShareDemo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ShareDemo;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ ShareDemo/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace ShareDemo;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        await using Stream inputStream = await FileSystem.Current.OpenAppPackageFileAsync("dotnet_bot.png");
+        string targetFile = Path.Combine(FileSystem.Current.AppDataDirectory, "dotnet_bot.png");
+
+        using FileStream outputStream = File.Create(targetFile);
+        await inputStream.CopyToAsync(outputStream);
+        outputStream.Close();
+        await Share.Default.RequestAsync(new ShareFileRequest
+        {
+            Title = "Share Image",
+            File = new ShareFile(targetFile)
+        });
+    }
+}
